c8dcecd8ae4e48799c052ae80e6d14cc694e6620
mmihye <smilehye910@naver.com>
2025-09-25 11:01:54 +0900

    상품 폴더

---
+++ src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,14 @@
+package com.sparta.myselectshop.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.User;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+	List<Folder> findAllByUser(User user);
+	List<Folder> findAllByUserAndNameIn(User user, List<String> folderNames);
+
+}
